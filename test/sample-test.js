a83a2ab542f94443a4bbad88f5888410537a0fc8
DemonWizard <demonwizard016@gmail.com>
2023-03-02 15:45:31 +0900

    Finished

---
+++ test/sample-test.js
@@ -0,0 +1,27 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
+
+describe("Staker", function () {
+  it("Staking test", async function () {
+    const Staker = await ethers.getContractFactory("Staker");
+    const StakerContract = await Staker.deploy("")
+
+  });
+});
